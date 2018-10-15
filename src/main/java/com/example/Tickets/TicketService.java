c0b0a07496524280fd2c5f878732f2d49272b10f
Agata Michalska <agatamichalska1988@gmail.com>
2018-10-15 22:46:21 +0200

    add new methods

--- src/main/java/com/example/Tickets/TicketService.java
+++ src/main/java/com/example/Tickets/TicketService.java
@@ -3,7 +3,6 @@ package com.example.Tickets;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,23 +11,21 @@ public class TicketService {
     @Autowired
     private TicketRepository ticketRepository;
 
-    private static List<Ticket> list = new ArrayList<>();
-
-    static {
-        list.add(new Ticket("Login error",TicketState.CREATED));
-        list.add(new Ticket("The search doesn't work",TicketState.PROGRESS));
-        list.add(new Ticket("E-mails sending error",TicketState.COMPLETED));
-    }
-
-    public List<Ticket> getList() {
+    public List<Ticket> getAllTickets() {
         return ticketRepository.findAll();
     }
 
-    public Ticket getByMessage(String message){
+    public Ticket getByMessage(String message) {
         return ticketRepository.findByMessage(message);
     }
 
     public void saveTicket(Ticket t) {
         ticketRepository.save(t);
     }
+
+    public void remove(Ticket id) {
+        ticketRepository.delete(id);
+    }
+
+    public void edit(){}
 }
