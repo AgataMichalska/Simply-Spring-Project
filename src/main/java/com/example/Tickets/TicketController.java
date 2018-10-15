9bd719a5914c62a37cc3997be4412e4ee02937ca
Agata Michalska <agatamichalska1988@gmail.com>
2018-10-15 22:49:59 +0200

    add new requests

--- src/main/java/com/example/Tickets/TicketController.java
+++ src/main/java/com/example/Tickets/TicketController.java
@@ -1,9 +1,9 @@
 package com.example.Tickets;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TicketController {
@@ -11,9 +11,25 @@ public class TicketController {
     @Autowired
     private TicketService ticketService;
 
+    @GetMapping("/tickets-search")
+    public Ticket getByMessage(@RequestParam("message") String message) {
+        return ticketService.getByMessage(message);
+    }
+
+    @GetMapping("/tickets")
+    public List<Ticket> getAllTickets() {
+        return ticketService.getAllTickets();
+    }
+
 
     @PostMapping("/tickets")
     public void postBook(@RequestBody Ticket t) {
         ticketService.saveTicket(t);
+    }
+
+
+    @DeleteMapping("/tickets/{id}")
+    public void deleteTicket(@PathVariable("id") Ticket ticketId) {
+        ticketService.remove(ticketId);
     }
 }
